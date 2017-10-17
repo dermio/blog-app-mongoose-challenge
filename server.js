39ae59cb4ded95d252abbcfad51d561b8cc40007
dermio <thisdjay@gmail.com>
2017-10-17 14:28:53 -0700

    Add promise library, GET route for single id

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+mongoose.Promise = global.Promise;
+app.use(bodyParser.json());
+
 app.get("/", function (req, res) {
   res.send("The app is working. Hello World.");
+});
+
+app.get("/:id", function (req, res) {
+  res.send(`GET request, id: ${req.params.id}`);
 });
 
 app.listen(process.env.PORT || 3000, function () {
