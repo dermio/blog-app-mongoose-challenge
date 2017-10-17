9340cd54876758e79294b942c813ea838db859dd
dermio <thisdjay@gmail.com>
2017-10-16 20:10:39 -0700

    Add GET route to test app.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("The app is working. Hello World.");
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log(`Your app is listening on ${process.env.PORT || 3000}`);
 });
