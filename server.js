1dad8df41127ae1d643fc4101a324b91cb43b465
dermio <thisdjay@gmail.com>
2017-10-18 09:27:12 -0700

    Added post, put, delete endpoints.

--- server.js
+++ server.js
@@ -10,14 +10,26 @@ const {PORT, DATABASE_URL} = require('./config');
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 
-app.get("/", function (req, res) {
+app.get("/posts", function (req, res) {
   res.send("The app is working. Hello World.");
 });
 
-app.get("/:id", function (req, res) {
+app.get("/posts/:id", function (req, res) {
   res.send(`GET request, id: ${req.params.id}`);
 });
 
-app.listen(PORT, function (req, res) {
+app.post("/posts", function (req, res) {
+  res.send(`POST request made`);
+});
+
+app.put("/posts/:id", function (req, res) {
+  res.send(`PUT request, id: ${req.params.id}`);
+});
+
+app.delete("/posts/:id", function (req, res) {
+  res.send(`DELETE request, id: ${req.params.id}`);
+});
+
+app.listen(PORT, function () {
   console.log(`Your app is listening on ${PORT}`);
 });
