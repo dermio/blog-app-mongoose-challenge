4aaa93e7c2c74b49f4bbac3e3a8b207d929e48db
dermio <thisdjay@gmail.com>
2017-10-18 19:19:24 -0700

    Added blogpost schema.

--- models.js
+++ models.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
 
+const blogpostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  author: {
+    firstName: String,
+    lastName: String
+  },
+  content: {type: String, required: true},
+});
+
+const Blogpost = mongoose.model("Blogpost", blogpostSchema);
+
+module.exports = {Blogpost};
