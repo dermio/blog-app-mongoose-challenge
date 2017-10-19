4db4d898c8d9946b76f8b7cbb8660d27f0e0c02e
dermio <thisdjay@gmail.com>
2017-10-19 11:07:00 -0700

    Fixed local url for mongodb.

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
-// Ask Kristen about: || "mongodb://localhost/some-app"
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL
+                        || "mongodb://localhost/blog-app-mongoose";
+
+// for the local url, make sure the database name in the url is the same
+// as the database name stored in Mongo
+// For example: blog-app-mongoose in Mongo, and
+// "mongodb://localhost/blog-app-mongoose" in the url
 
 exports.PORT = process.env.PORT || 8080;
